f8616902a1b90edeb6bac00520fe1c79562e52d3
Joerg-Schultz <joergsky@online.de>
2022-03-31 15:41:49 +0200

    configured sign in client with app key in confidential (gitignore) file

--- app/src/main/java/de/tierwohlteam/android/understandgooglelogin/ui/main/MainFragment.kt
+++ app/src/main/java/de/tierwohlteam/android/understandgooglelogin/ui/main/MainFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import de.tierwohlteam.android.understandgooglelogin.R
+import de.tierwohlteam.android.understandgooglelogin.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -16,11 +20,38 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    // https://developers.google.com/identity/one-tap/android/get-saved-credentials#kotlin
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        oneTapClient = Identity.getSignInClient(view.context) // does this have to be an activity??
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
